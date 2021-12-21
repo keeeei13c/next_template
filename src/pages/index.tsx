8dd8896f9c6e2f2bdb10c1502a55cc052852a78e
keeeei13c <kei.121003f@icloud.com>
2021-12-21 19:17:04 +0900

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+export default function Index() {
+  return (
+    <>
+      <div className="max-w-md mx-auto bg-gray-300">
+        <div>templete file</div>
+      </div>
+    </>
+  );
+}
